674b02ce3eeffc0143827c5ba7f8084d28b3f99e
Gemala Putri Mulgiati <gemalaputri26@gmail.com>
2019-01-17 18:25:37 +0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Switch } from 'react-router'
+import history from './dependencies/history'
+import { Route, Router } from 'react-router-dom'
 
 import Products from './scenes/Products'
 
 class App extends Component {
 	render() {
 		return (
-			<Products />
+			<Router history={history}>
+		    <Switch>
+		      <Route path='/' exact component={Products} />
+		    </Switch>
+		  </Router>
 		)
 	}
 }
