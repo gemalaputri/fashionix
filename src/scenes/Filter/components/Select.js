c2346b8d963606273bd72dd6c7334e73e45c89a5
Gemala Putri Mulgiati <gemalaputri26@gmail.com>
2019-01-20 01:12:45 +0700

    change filtered data by default and by size

--- src/scenes/Filter/components/Select.js
+++ src/scenes/Filter/components/Select.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
+import { Map, List, fromJS } from 'immutable'
 import {
   productsFilterBySize,
   productsFilterByPrice,
   productsClearFilterData,
 } from '../actions'
+import { fetchProducts, addFilteredProducts } from '../../Products/actions' //get actions from
 
 class Select extends Component {
   constructor(props) {
@@ -31,7 +33,11 @@ class Select extends Component {
       productsClearFilterData()
       .then(() => {
         productsFilterBySize(true)
+        .then(() => {
+          this.handleChangeFilterSize() //call the function to sorting the products by sizeChart
+        })
       })
+
     } else if(e.target.value === 'price') {
       productsClearFilterData()
       .then(() => {
@@ -39,8 +45,61 @@ class Select extends Component {
       })
     } else {
       productsClearFilterData()
-      console.log("clear");
+      .then(() => {
+        this.handleChangeClearFilter() //call the function to sorting the products by index
+      })
     }
+  }
+
+  handleChangeClearFilter = () => {
+    const { fetchProducts } = this.props
+
+    const _products = this.props.products.sort(function(a,b){return a.get('index')-b.get('index')})
+
+    fetchProducts(_products) //dispatch to products
+  }
+
+  handleChangeFilterSize = () => {
+    const { products, addFilteredProducts, fetchProducts } = this.props
+
+    const sizeChart = ["XS","S","M","L","XL"] //assign a number for each string
+    const _sortable = products.map( product => {
+      const _sort = sizeChart.findIndex( k => k === product.get('size').get(0))
+      return fromJS({
+        index: product.get('index'),
+        sort: _sort
+      })
+    })
+    {/* we take the first string of size
+      * check the string and give the number based our sizeChart
+      * create new object contain the "product id" and number of sizeChart in "sort" key
+      * the _sortable object is not sorted yet
+      */}
+    const result = _sortable.sort(function(a,b){return a.get('sort')-b.get('sort')}) //sorted _sortable
+
+    addFilteredProducts(result) // dispatch to be productsIndexes
+    .then(() => {
+      let _productFiltered = [] // new array for sorted product
+      const _productsIndexes = this.props.productsIndexes.map( key => {
+        const _products = products.filter( item => {
+          if(item.get('index') === key.get('index')) {
+            _productFiltered.push(item)
+            return false
+          } else {
+            return true
+          }
+        }).first()
+      })
+      {/* we already have productsIndexes with "product id as index"
+        * filter existing/old products with mapping it first
+        * check if the existing product id is equal to product id in productsIndexes
+        * then push the product content to new array of products
+        * we sorted new products by productsIndexes.
+        */}
+
+      fetchProducts(fromJS(_productFiltered))
+      // dispatch new products by productsIndexes to replace existing products
+    })
   }
 
   render () {
@@ -56,7 +115,9 @@ class Select extends Component {
 
 const mapStateToProps  = (state) => {
   return {
-    productsFilterData: state.productsFilterData
+    products: state.products, //products data from redux state
+    productsFilterData: state.productsFilterData,
+    productsIndexes: state.productsIndexes
   }
 }
 
@@ -65,6 +126,8 @@ const mapDispatchToProps  = (dispatch) => {
     productsFilterBySize: (size) => dispatch(productsFilterBySize(size)),
     productsFilterByPrice: (price) => dispatch(productsFilterByPrice(price)),
     productsClearFilterData: () => dispatch(productsClearFilterData()),
+    fetchProducts: (products) => dispatch(fetchProducts(products)), //activate fetchProducts from actions for update redux state
+    addFilteredProducts: (indexes) => dispatch(addFilteredProducts(indexes)),
   }
 }
 
